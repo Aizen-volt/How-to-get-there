4865fd4b11ec188a0b619dc4b1aa4be5c41fd05e
Mateusz Kaszubowski <119121964+Aizen-volt@users.noreply.github.com>
2023-05-01 13:19:18 +0200

    changed reallocation factor

--- Vector.h
+++ Vector.h
@@ -5,82 +5,93 @@
 #include <iostream>
 #include <cmath>
 
-#define CELLS_EXCESS_WHEN_REALLOCATING 8192
+#define CELLS_EXCESS_WHEN_REALLOCATING 40000
 
 template <typename T> class Vector {
 private:
     T* array;
     int size; //number of used cells
     int capacity; //number of cells to use before reallocating
-    int excessFactor;
 
 
     void CheckCapacity() {
         if (size == capacity)
-            ChangeCapacity(size + excessFactor);
-    }
+            ChangeCapacity(size + CELLS_EXCESS_WHEN_REALLOCATING);
+    
+}
 
 
     void ChangeCapacity(int target_capacity) {
         T* temp = new T[target_capacity];
         for (int i = 0; i < capacity; i++) {
             temp[i] = array[i];
-        }
+        
+}
         capacity = target_capacity;
         delete[] array;
         array = temp;
-    }
+    
+}
 public:
-    Vector(int excessFactor = CELLS_EXCESS_WHEN_REALLOCATING) {
+    Vector() {
         size = 1;
-        capacity = excessFactor;
+        capacity = CELLS_EXCESS_WHEN_REALLOCATING;
         array = new T[capacity];
-    }
+    
+}
 
 
     Vector(const Vector<T>& other) {
         size = other.size;
         capacity = other.capacity;
         array = other.array;
-    }
+    
+}
 
 
     ~Vector() {
         delete[] array;
-    }
+    
+}
 
 
     T& operator[](int index) {
         return array[index];
-    }
+    
+}
 
 
     void Push_back(T& data) {
         size++;
         CheckCapacity();
         array[size - 2] = data;
-    }
+    
+}
 
 
     void Push_back() {
         size++;
         CheckCapacity();
-    }
+    
+}
 
 
     void Pop_back() {
         size--;
         CheckCapacity();
-    }
+    
+}
 
 
     [[nodiscard]] int Size() const {
         return size - 1;
-    }
+    
+}
 
     T* GetArray() const {
         return array;
-    }
+    
+}
 };
 
 #endif //NEWMAPPER_VECTOR_H
