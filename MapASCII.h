342159e0a3cc900cbcef97b67ef034abe2da0abd
Mateusz Kaszubowski <119121964+Aizen-volt@users.noreply.github.com>
2023-11-12 11:46:30 +0100

    added isOutOfBound()

--- MapASCII.h
+++ MapASCII.h
@@ -10,39 +10,50 @@ class MapASCII {
 
 private:
     [[nodiscard]] static bool isCityName(char c) {
-        return isalpha(c) || isdigit(c);
+        return std::isalpha(c) || std::isdigit(c);
     }
 
 
-    String findCityName(int x, int y) {
+    [[nodiscard]] bool isOutOfBound(int x, int y) const {
+        return x < 0 || x >= width || y < 0 || y >= height;
+    }
+
+
+    [[nodiscard]] String findCityName(int x, int y) const {
         String name;
 
+        // check cells surrounding city entrance for its name
         for (int offX = -1; offX <= 1; offX++) {
-            if (x + offX < 0 || x + offX >= width) {
+            // out of bound
+            if (isOutOfBound(x + offX, y)) {
                 continue;
             }
 
             for (int offY = -1; offY <= 1; offY++) {
-                if (y + offY < 0 || y + offY >= height) {
+                if (isOutOfBound(x, y + offY)) {
                     continue;
                 }
+                // skip city entrance
                 if (offX == 0 && offY == 0) {
                     continue;
                 }
 
+                // letter found
                 if (isCityName(array[x + offX][y + offY])) {
-                    // last letter
-                    if (x + offX + 1 >= width || !isCityName(array[x + offX + 1][y + offY])) {
-                        while (x + offX > 0 && isCityName(array[x + offX][y + offY])) {
+                    // last letter of name
+                    if (isOutOfBound(x + offX + 1, y + offY) || !isCityName(array[x + offX + 1][y + offY])) {
+                        // start from last letter and go backwards
+                        while (!isOutOfBound(x + offX, y + offY) && isCityName(array[x + offX][y + offY])) {
                             name.pushFront(array[x + offX][y + offY]);
                             offX--;
                         }
                         return name;
                     }
 
-                    // first letter
-                    else if (x + offX - 1 < 0 || !isCityName(array[x + offX - 1][y + offY])) {
-                        while (x + offX < width && isCityName(array[x + offX][y + offY])) {
+                    // first letter of name
+                    if (isOutOfBound(x + offX - 1, y + offY) || !isCityName(array[x + offX - 1][y + offY])) {
+                        // start from first letter and go forwards
+                        while (!isOutOfBound(x + offX, y + offY) && isCityName(array[x + offX][y + offY])) {
                             name.pushBack(array[x + offX][y + offY]);
                             offX++;
                         }
@@ -58,17 +69,17 @@ public:
     static const char ROAD_SYMBOL = '#';
     static const char CITY_SYMBOL = '*';
 
-    char** array;
+    char **array;
     int width, height;
     int roadCount;
 
     Vector<Position> cityPositions;
     Vector<String> cityNames;
-    Map cityNameToId;
+    Map cityNameToId; // city name -> city id for fast access in queries
 
 
     MapASCII(int width, int height) : width(width), height(height) {
-        array = new char*[width];
+        array = new char *[width];
         for (int i = 0; i < width; i++) {
             array[i] = new char[height];
         }
@@ -89,6 +100,7 @@ public:
         char cell;
         String line;
 
+        // read from input, count roads and mark city entrances
         for (int y = 0; y < height; y++) {
             std::cin >> line;
             for (int x = 0; x < width; x++) {
@@ -96,21 +108,20 @@ public:
 
                 if (cell == ROAD_SYMBOL) {
                     roadCount++;
-                }
-                else if (cell == CITY_SYMBOL) {
+                } else if (cell == CITY_SYMBOL) {
                     cityPositions.pushBack(Position(x, y));
                 }
                 array[x][y] = cell;
             }
         }
 
+        // for each city entrance find its name
         String cityName;
         for (int i = 0; i < cityPositions.getSize(); i++) {
             cityName = findCityName(cityPositions[i].x, cityPositions[i].y);
             cityNames.pushBack(cityName);
             cityNameToId.add(cityName, i);
         }
-
     }
 };
 
