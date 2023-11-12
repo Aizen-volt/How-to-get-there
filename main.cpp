d9d5e4f9016ebd54e09970219d9fae044716383f
Mateusz Kaszubowski <m.aizenvolt@gmail.com>
2023-11-12 12:26:01 +0100

    added comments

--- main.cpp
+++ main.cpp
@@ -5,19 +5,25 @@
 #include "Graph.h"
 
 int main() {
+    // for measuring execution time
     auto begin = std::chrono::high_resolution_clock::now();
 
+    // speed up i/o
     std::ios_base::sync_with_stdio(false);
     std::cin.tie(nullptr);
 
+    // read map dimensions
     int width, height;
     std::cin >> width >> height;
 
+    // read map
     MapASCII mapAscii(width, height);
     mapAscii.formMap();
 
+    // create graph
     Graph graph(&mapAscii);
 
+    // read potential flight connections
     int flightConnections;
     std::cin >> flightConnections;
 
@@ -25,6 +31,7 @@ int main() {
         graph.readFlightConnection();
     }
 
+    // handle queries
     int queriesCount;
     std::cin >> queriesCount;
 
@@ -32,6 +39,7 @@ int main() {
         graph.printShortestPath();
     }
 
+    // print execution time
     auto end = std::chrono::high_resolution_clock::now();
     auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin);
     std::cout << "\n" << elapsed.count() * 1e-9;
