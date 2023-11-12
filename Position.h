95b588300924cbeea025516d3d72825b0ef38c05
Mateusz Kaszubowski <119121964+Aizen-volt@users.noreply.github.com>
2023-11-12 01:05:37 +0100

    Create Position.h

---
+++ Position.h
@@ -0,0 +1,12 @@
+#ifndef HOW_TO_GET_THERE_POSITION_H
+#define HOW_TO_GET_THERE_POSITION_H
+
+class Position {
+
+public:
+    int x, y;
+
+    explicit Position(int x = -1, int y = -1) : x(x), y(y) {}
+};
+
+#endif //HOW_TO_GET_THERE_POSITION_H
